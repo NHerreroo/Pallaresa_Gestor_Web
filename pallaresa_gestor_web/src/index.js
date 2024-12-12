d9573114b4179b4c9200d884e115c02f08a9b406
NachoHerrero <116599852+NHerreroo@users.noreply.github.com>
2024-12-12 09:45:37 +0100

    generic lobby

--- pallaresa_gestor_web/src/index.js
+++ pallaresa_gestor_web/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GenericLobby } from './Pages/Others/Js/GenericLobby';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />
+  //<App />
+  <GenericLobby/>
+
 );
 
 reportWebVitals();
