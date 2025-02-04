f0e57f8b6c193ac182d3fc530a7a7bf83f6bfebb
NachoHerrero <116599852+NHerreroo@users.noreply.github.com>
2025-02-04 11:26:56 +0100

    Works

--- pallaresa_gestor_web/Server/server.js
+++ pallaresa_gestor_web/Server/server.js
@@ -8,43 +8,6 @@ const port = 3001;
 app.use(cors());
 app.use(express.json());
 
-<<<<<<< Updated upstream
-
-// Autenticar usuario y verificar rol de administrador
-app.post("/login", async (req, res) => {
-    const { correo, contraseña } = req.body;
-    try {
-        const userQuery = `
-            SELECT p.nombre, pr.nombre_rol 
-            FROM personas p
-            JOIN persona_rol pr ON p.correo = pr.correo_persona
-            WHERE p.correo = $1 AND p.contraseña = $2 AND pr.nombre_rol = 'Administrador'
-        `;
-        const result = await pool.query(userQuery, [correo, contraseña]);
-
-        if (result.rows.length > 0) {
-            res.status(200).json({ success: true, nombre: result.rows[0].nombre });
-        } else {
-            res.status(401).json({ success: false, message: "Credenciales incorrectas o no tiene rol de administrador" });
-        }
-    } catch (err) {
-        console.error(err);
-        res.status(500).send("Error al iniciar sesión");
-    }
-});
-
-
-
-// CRUD Usuarios
-app.post("/usuarios", async (req, res) => {
-    const { correo, nombre, contraseña } = req.body;
-    try {
-        await pool.query("INSERT INTO personas (correo, nombre, contraseña) VALUES ($1, $2, $3)", [correo, nombre, contraseña]);
-        res.status(201).send("Usuario creado correctamente");
-    } catch (err) {
-        console.error(err);
-        res.status(500).send("Error al crear usuario");
-=======
 app.post('/api/register', async (req, res) => {
   const { correo, nombre, contraseña, rol } = req.body;
   try {
@@ -60,7 +23,6 @@ app.post('/api/register', async (req, res) => {
         'INSERT INTO persona_rol (correo_Persona, nombre_Rol) VALUES ($1, $2)',
         [correo, rol]
       );
->>>>>>> Stashed changes
     }
 
     res.status(201).json({ message: 'Usuario registrado', usuario: resultPersona.rows[0] });
