bd8279c18713a38c1bb019794f20409582b7e864
BenPerdeG <bperez@elpuig.xeill.net>
2025-03-06 12:10:07 +0100

    l

--- pallaresa_gestor_web/src/index.js
+++ pallaresa_gestor_web/src/index.js
@@ -3,12 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import ModalRol from './Pages/Admin/JS/ModalRol';
-import CrearUsuario from './Pages/Admin/JS/CrearUsuario';
-import EditarUsuario from './Pages/Admin/JS/EditarUsuario';
-import Scroll from './componentes/JS/Scroll'
-
-import { GenericLobby } from './Pages/Others/Js/GenericLobby';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
