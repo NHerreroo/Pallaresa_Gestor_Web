6b2e509c8576cd4d13e423bc95c6e33d477a177c
BenPerdeG <bperez@elpuig.xeill.net>
2025-03-12 10:52:37 +0100

    botones topbar fix

--- pallaresa_gestor_web/src/componentes/JS/TopBar.js
+++ pallaresa_gestor_web/src/componentes/JS/TopBar.js
@@ -1,21 +1,26 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom'; // Importa useLocation
 import '../Css/TopBar.css';
-import {UserButton} from '../JS/UserButton.js';
-import {FolderButton} from '../JS/FolderButton.js';
-
+import { UserButton } from '../JS/UserButton.js';
+import { FolderButton } from '../JS/FolderButton.js';
 
 export const TopBar = () => {
+  const location = useLocation(); // Obtiene la ruta actual
+
   return (
     <div>
       <div className='TBarra'>
-      <div className='BotonU'>
-        <UserButton/>
+        {/* Muestra UserButton si NO estamos en "/buscar-usuarios" */}
+        <div className={`BotonU ${location.pathname === '/admin/users' ? 'hidden' : ''}`}>
+          <UserButton />
+        </div>
+        {/* Muestra FolderButton SI estamos en "/buscar-usuarios" */}
+        <div className={`BotonF ${location.pathname === '/admin/users' ? '' : 'hidden'}`}>
+          <FolderButton />
+        </div>
       </div>
-      <div className='BotonF'>
-        <FolderButton/>
-      </div>
-      </div>
-      </div>
+    </div>
   );
 };
+
 export default TopBar;
